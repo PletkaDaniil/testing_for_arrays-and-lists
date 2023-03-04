68c6bb7c7d478762915474c17b0f354775726991
Pletka Daniil <pletkadaniil@yandex.ru>
2023-03-04 12:38:31 +0300

    new commit

--- testing for lists.cpp
+++ testing for lists.cpp
@@ -1,28 +1,26 @@
 #include <gtest/gtest.h>
-#include "arry.h"
+#include "lis.h"
 
 TEST(ListTest, ListCreate) { // При создании массива его размер должен быть нулевым
-	list a;
+	Lis a;
 	ASSERT_EQ(a.getSize(), 0) << "Size of empty array should be 0";
 }
 
 TEST(ListTest, ListSetElem) { // Проверяем замену элементов
-	list a;
-	a.addElement(5);
+	Lis a;
+	a.addElement(17);
 	a.addElement(6);
-	a.addElement(19);
-	a.addElement(18);
-	a.setElement(1, 4);
-	//EXPECT_EQ(a.getElement(0), 7);
-	ASSERT_EQ(a.getElement(1), 5);
-	//ASSERT_EQ(a.getElement(2), 7);
+	a.addElement(96);
+	a.addElement(105);
+	a.setCurrent(5);
+	ASSERT_EQ(a.getCurrent(), 5);
 }
 
 TEST(ArrayTest, ListAddElem) { // Проверяем добавление элемента
-	list a;
+	Lis a;
 	a.addElement(42);
 	ASSERT_EQ(a.getSize(), 1);
-	ASSERT_EQ(a.getElement(0), 42);
+	ASSERT_EQ(a.getCurrent(), 42);
 }
 
 int main(int argc, char* argv[]) {
